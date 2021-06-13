2c949e797da9d25794a4fb164f4dd5abc20f2b75
gin0115 <glynn.quelch@gmail.com>
2021-06-14 00:49:48 +0100

    House Keeping

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# PinkCrab Plugin Framework Builder
+# PinkCrab Perique Boilerplate Builder
 
 **You can also build this from the current binary**
 ``` $ wget https://bin.pinkcrab.co.uk/pinkcrab  && php pinkcrab```
 
 
-# Version 0.4.0
-> Currently compatible with V0.4.* of the Plugin Boilerplate (which uses 0.4.* version of the core codebase)
+# Version 1.0.0
+> Currently compatible with V1.\*.\* of the Perique Boilerplate (which it self uses V1.\*.\* of the **Perique Framework**)
 
-This is a simple executeable which can be used to create an fully functional version of the PinkCrab Plugin Framework
+This is a simple executable which can be used to create an fully functional version of the PinkCrab Plugin Framework
 
-To create a new plugin just copy the pinkcrab executeable from the bin to your empty plugin dir and run ```./pinkcrab```
+To create a new plugin just copy the pinkcrab executable from the bin to your empty plugin dir and run ```./pinkcrab```
 
-You will then be guided through the setup process, where you will asked a number of questions to help setup the plugin. Once this has all been completed, the plugin should be created and the inital executeable will be removed.
+You will then be guided through the setup process, where you will asked a number of questions to help setup the plugin. Once this has all been completed, the plugin should be created and the initial executable will be removed.
 
-This will clone in a full version of the plugin boilerplate, composer install has not been run yet, so feel free to add all additional depenedencies. Once you are happy and ready we can install our dependencies. Do not however run composer install!!
+This will clone in a full version of the plugin boilerplate, composer install has not been run yet, so feel free to add all additional dependencies. Once you are happy and ready we can install our dependencies. Do not however run composer install!!
 
 ```bash
 # RUN ME FOR composer install
@@ -28,15 +28,15 @@ $ ./build.sh
 
 The version of the boilerplate installed, comes set with (slightly clunky) config for using PHP Scoper. This allows you to create your plugin with a unique namespace, not only for your own code, but all the dependencies you are using. This removes the potential for conflicts with other plugins which could be using an older or newer version of a library.
 
-When the custom build.sh script is run, ```composer install``` will be run, this will create our normal vendor directory and autloader. Then a patcher of all functions, classes and constants are taken for WP Core, WooCommerce and ACF (all latest versions). Then ```composer install --no-dev``` is run to give us a production build. PHP Scoper is then run, using the WP, WooCommerce and ACF patcher and a custom  vendor directory and autoloader is created (in build/vendor). Then we run ```composer install``` again, to give us back our intial vendor and autoloader.
+When the custom build.sh script is run, ```composer install``` will be run, this will create our normal vendor directory and autloader. Then a patcher of all functions, classes and constants are taken for WP Core, WooCommerce and ACF (all latest versions). Then ```composer install --no-dev``` is run to give us a production build. PHP Scoper is then run, using the WP, WooCommerce and ACF patcher and a custom  vendor directory and autoloader is created (in build/vendor). Then we run ```composer install``` again, to give us back our initial vendor and autoloader.
 
 ### WHY 3 COMPOSER INSTALLS!!!!
 
-Well, we could have skipped the last 2 composer installs and just created a custom vendor/autloader using the dev dependencies, but thats totally pointless as most depenecies are for testing. So after we run the first dev build, we use static anaylis stubs for our WP/WC/ACF **patchers** (all functions, classes and constants TO NOT apply a custom namespace to). So once we generate them, we run composer isntall for a dev build, scope that and then run again so we can use phpunit, php-stan and phpcs.
+Well, we could have skipped the last 2 composer installs and just created a custom vendor/autloader using the dev dependencies, but thats totally pointless as most dependencies are for testing. So after we run the first dev build, we use static analysis stubs for our WP/WC/ACF **patchers** (all functions, classes and constants TO NOT apply a custom namespace to). So once we generate them, we run composer install for a dev build, scope that and then run again so we can use phpunit, php-stan and phpcs.
 
 ### So Which Copy
 
-As we have 2 sets of vendor and autoloaders, you need be careful which you choose to use in your code. During the setup process it will have asked you for a the namespace prefix to use. Always choose the pefixed one in your production code (/src directory)
+As we have 2 sets of vendor and autoloaders, you need be careful which you choose to use in your code. During the setup process it will have asked you for a the namespace prefix to use. Always choose the prefixed one in your production code (/src directory)
 
 ```php
 use My_Custom_Prefix\PinkCrab\Core\...;
--- src/Command/Build.php
+++ src/Command/Build.php
@@ -20,40 +20,40 @@ use PinkCrab\Plugin_Boilerplate_Builder\Builder\PluginDetails;
 use PinkCrab\Plugin_Boilerplate_Builder\Builder\PluginSetting;
 use PinkCrab\Plugin_Boilerplate_Builder\Git\Repository as GitRepository;
 
-class Build extends Command
-{
+class Build extends Command {
 
 
-    protected PluginBuilder $pluginBuilder;
-    protected Application $app;
-    protected CommandStyles $styles;
-    protected GitRepository $gitRepository;
-    protected Settings $settings;
-    protected FilePopulator $filePopulator;
-    protected Filesystem $fileSystem;
 
-    public function __construct(
-        Application $app,
-        Settings $settings,
-        CommandStyles $styles,
-        PluginBuilder $pluginBuilder,
-        GitRepository $gitRepository,
-        Filesystem $fileSystem,
-        FilePopulator $filePopulator
-    ) {
-        $this->app           = $app;
-        $this->styles        = $styles;
-        $this->settings      = $settings;
-        $this->gitRepository = $gitRepository;
-        $this->pluginBuilder = $pluginBuilder;
-        $this->fileSystem    = $fileSystem;
-        $this->filePopulator = $filePopulator;
-    }
+	protected PluginBuilder $pluginBuilder;
+	protected Application $app;
+	protected CommandStyles $styles;
+	protected GitRepository $gitRepository;
+	protected Settings $settings;
+	protected FilePopulator $filePopulator;
+	protected Filesystem $fileSystem;
 
-    /**
-     * @var string
-     */
-    private string $logo = <<<LOGO
+	public function __construct(
+		Application $app,
+		Settings $settings,
+		CommandStyles $styles,
+		PluginBuilder $pluginBuilder,
+		GitRepository $gitRepository,
+		Filesystem $fileSystem,
+		FilePopulator $filePopulator
+	) {
+		$this->app           = $app;
+		$this->styles        = $styles;
+		$this->settings      = $settings;
+		$this->gitRepository = $gitRepository;
+		$this->pluginBuilder = $pluginBuilder;
+		$this->fileSystem    = $fileSystem;
+		$this->filePopulator = $filePopulator;
+	}
+
+	/**
+	 * @var string
+	 */
+	private string $logo = <<<LOGO
 
             ______  _         _     _____               _             ______              _                         
             | ___ \(_)       | |   /  __ \             | |            | ___ \            (_)                        
@@ -72,162 +72,160 @@ class Build extends Command
                                         |_|                                                                                                                                        
 LOGO;
 
-    /**
-     * @return string
-     */
-    private function title(): string
-    {
-        return <<<TITLE
+	/**
+	 * @return string
+	 */
+	private function title(): string {
+		return <<<TITLE
             	                                                                                             v{$this->settings->getAppVersion()}
 TITLE;
-    }
+	}
 
-    /**
-     * @param OutputInterface $output
-     * @param Input $input
-     * @return void
-     */
-    public function __invoke(
-        bool $dev = false,
-        bool $prod = false,
-        OutputInterface $output, //phpcs:disable PEAR.Functions.ValidDefaultValue.NotAtEnd -- no control over arg order
-        Input $input
-    ) {
+	/**
+	 * @param OutputInterface $output
+	 * @param Input $input
+	 * @return void
+	 */
+	public function __invoke(
+		bool $dev = false,
+		bool $prod = false,
+		OutputInterface $output, //phpcs:disable PEAR.Functions.ValidDefaultValue.NotAtEnd -- no control over arg order
+		Input $input
+	) {
 
-        // Show intro screen.
-        $output = $this->styles->registerStyles($output);
-        $output->writeln('<darkpink>' . $this->logo . '</>');
-        $output->writeln('<lightpink>' . $this->title() . PHP_EOL . '</>');
+		// Show intro screen.
+		$output = $this->styles->registerStyles( $output );
+		$output->writeln( '<darkpink>' . $this->logo . '</>' );
+		$output->writeln( '<lightpink>' . $this->title() . PHP_EOL . '</>' );
 
-        // Ask questions
-        $this->pluginBuilder->askQuestions($input, $output, $this->app->getHelperSet());
+		// Ask questions
+		$this->pluginBuilder->askQuestions( $input, $output, $this->app->getHelperSet() );
 
-        // Abort if any errors.
-        $this->verifyPluginDetails($output);
+		// Abort if any errors.
+		$this->verifyPluginDetails( $output );
 
-         // Compose translations.
-        $translations = $this->pluginBuilder->pluginDetails()->asTranslationArray();
-        $outputHelper = $this->styles->getStyler($input, $output);
+		 // Compose translations.
+		$translations = $this->pluginBuilder->pluginDetails()->asTranslationArray();
+		$outputHelper = $this->styles->getStyler( $input, $output );
 
-        // Show details on screen.
-        $outputHelper->table(
-            array( 'Placeholder', 'Inputted Value' ),
-            $this->pluginBuilder->pluginDetails()->asPlaceholderList()
-        );
+		// Show details on screen.
+		$outputHelper->table(
+			array( 'Placeholder', 'Inputted Value' ),
+			$this->pluginBuilder->pluginDetails()->asPlaceholderList()
+		);
 
-        // Verify user input.
-        if (! $outputHelper->confirm('<darkpink>Are your details corrent?</>')) {
-            $outputHelper->caution('USER ABORTED PROCESS');
-            exit;
-        }
+		// Verify user input.
+		if ( ! $outputHelper->confirm( '<darkpink>Are your details corrent?</>' ) ) {
+			$outputHelper->caution( 'USER ABORTED PROCESS' );
+			exit;
+		}
 
-        // Clone repo & remove .git
-        $this->cloneRepoToTempDirectory();
+		// Clone repo & remove .git
+		$this->cloneRepoToTempDirectory();
 
-        // Replace all placehoders.
-        if (! $this->populateFiles($translations)) {
-            $output->writeln('<error>FAILED TO POPULATE MAIN FILES. PLEASE TRY AGAIN</>');
-            exit;
-        }
+		// Replace all placehoders.
+		if ( ! $this->populateFiles( $translations ) ) {
+			$output->writeln( '<error>FAILED TO POPULATE MAIN FILES. PLEASE TRY AGAIN</>' );
+			exit;
+		}
 
-        // Replace placeholders in composer.json
-        if (! $this->populateComposer($translations)) {
-            $output->writeln('<error>FAILED TO POPULATE COMPOSER.JSON, ABORTED OPERATION. PLEASE TRY AGAIN</>');
-            exit;
-        }
+		// Replace placeholders in composer.json
+		if ( ! $this->populateComposer( $translations ) ) {
+			$output->writeln( '<error>FAILED TO POPULATE COMPOSER.JSON, ABORTED OPERATION. PLEASE TRY AGAIN</>' );
+			exit;
+		}
 
-        // Move files from temp to root.
-        $output->writeln('<lightpink>Moving files from temp to root directory');
-        $this->fileSystem->mirror(
-            $this->settings->getTempPath(),
-            $this->settings->getBasePath(),
-            null,
-            array( 'override' => true )
-        );
+		// Move files from temp to root.
+		$output->writeln( '<lightpink>Moving files from temp to root directory' );
+		$this->fileSystem->mirror(
+			$this->settings->getTempPath(),
+			$this->settings->getBasePath(),
+			null,
+			array( 'override' => true )
+		);
 
-        // Remove the empty temp directory.
-        $output->writeln('<lightpink>Cleaning up temp directory');
-        $this->fileSystem->remove($this->settings->getTempPath());
+		// Remove the empty temp directory.
+		$output->writeln( '<lightpink>Cleaning up temp directory' );
+		$this->fileSystem->remove( $this->settings->getTempPath() );
 
-        // Check if created from pinkcrab bin
-        if (file_exists($this->settings->getBasePath() . \DIRECTORY_SEPARATOR . 'pinkcrab')) {
-            $this->fileSystem->remove($this->settings->getBasePath() . \DIRECTORY_SEPARATOR . 'pinkcrab');
-            $output->writeln('<lightpink>Removed local builder binary');
-        }
+		// Check if created from pinkcrab bin
+		if ( file_exists( $this->settings->getBasePath() . \DIRECTORY_SEPARATOR . 'pinkcrab' ) ) {
+			$this->fileSystem->remove( $this->settings->getBasePath() . \DIRECTORY_SEPARATOR . 'pinkcrab' );
+			$output->writeln( '<lightpink>Removed local builder binary' );
+		}
 
-        $output->writeln('<darkpink>Plugin Built');
-    }
+		$output->writeln( '<darkpink>Plugin Built' );
+
+		$output->writeln( '<lightpink>Don\'t forget to run composer build-dev rather than composer install!' );
+
+	}
 
 
-    /**
-     * Verfifies that none of the plugin details has errors.
-     * Returns error to cli if found.
-     *
-     * @param OutputInterface $output
-     * @return void
-     */
-    protected function verifyPluginDetails(OutputInterface $output): void
-    {
-        if ($this->pluginBuilder->hasErrors()) {
-            $output->writeln(PHP_EOL);
-            foreach ($this->pluginBuilder->getErrors() as $error) {
-                $output->writeln("<error>{$error}</>");
-            }
-            $output->writeln('<error>BUILD TERMINATED</>');
-            $output->writeln(PHP_EOL);
-            exit;
-        }
-    }
+	/**
+	 * Verfifies that none of the plugin details has errors.
+	 * Returns error to cli if found.
+	 *
+	 * @param OutputInterface $output
+	 * @return void
+	 */
+	protected function verifyPluginDetails( OutputInterface $output ): void {
+		if ( $this->pluginBuilder->hasErrors() ) {
+			$output->writeln( PHP_EOL );
+			foreach ( $this->pluginBuilder->getErrors() as $error ) {
+				$output->writeln( "<error>{$error}</>" );
+			}
+			$output->writeln( '<error>BUILD TERMINATED</>' );
+			$output->writeln( PHP_EOL );
+			exit;
+		}
+	}
 
-    /**
-     * Clones the repo to the defined temp directory.
-     *
-     * @return void
-     */
-    protected function cloneRepoToTempDirectory(): void
-    {
-        $this->gitRepository->clone(
-            $this->settings->getRepoUri(),
-            $this->settings->getTempPath(),
-            $this->settings->getRepoBranch()
-        )->removeGitConfig($this->settings->getTempPath() . '/.git');
-    }
+	/**
+	 * Clones the repo to the defined temp directory.
+	 *
+	 * @return void
+	 */
+	protected function cloneRepoToTempDirectory(): void {
+		$this->gitRepository->clone(
+			$this->settings->getRepoUri(),
+			$this->settings->getTempPath(),
+			$this->settings->getRepoBranch()
+		)->removeGitConfig( $this->settings->getTempPath() . '/.git' );
+	}
 
-    /**
-     * Populates all the main files.
-     *
-     * @param array<string, string> $translations
-     * @return bool
-     */
-    protected function populateFiles(array $translations): bool
-    {
-        try {
-            $this->filePopulator->translateFiles(
-                $this->settings->getTempPath(),
-                $translations,
-                $this->settings->getExcludedDirectories(),
-                $this->settings->getExcludedFiles()
-            );
-        } catch (\Throwable $th) {
-            return false;
-        }
-        return true;
-    }
+	/**
+	 * Populates all the main files.
+	 *
+	 * @param array<string, string> $translations
+	 * @return bool
+	 */
+	protected function populateFiles( array $translations ): bool {
+		try {
+			$this->filePopulator->translateFiles(
+				$this->settings->getTempPath(),
+				$translations,
+				$this->settings->getExcludedDirectories(),
+				$this->settings->getExcludedFiles()
+			);
+		} catch ( \Throwable $th ) {
+			return false;
+		}
+		return true;
+	}
 
-    /**
-     * Populates composer.json
-     * uses double \\ on namespaces (well \\\\ with escaping)
-     *
-     * @param array<string, string> $translations
-     * @return bool
-     */
-    protected function populateComposer(array $translations): bool
-    {
-        $translations['##NAMESPACE##']    = Strings::doubleSlashes($translations['##NAMESPACE##']);
-        return $this->filePopulator->findAndPopulateFile(
-            'composer.json',
-            $translations,
-            $this->settings->getTempPath()
-        );
-    }
+	/**
+	 * Populates composer.json
+	 * uses double \\ on namespaces (well \\\\ with escaping)
+	 *
+	 * @param array<string, string> $translations
+	 * @return bool
+	 */
+	protected function populateComposer( array $translations ): bool {
+		$translations['##NAMESPACE##'] = Strings::doubleSlashes( $translations['##NAMESPACE##'] );
+		return $this->filePopulator->findAndPopulateFile(
+			'composer.json',
+			$translations,
+			$this->settings->getTempPath()
+		);
+	}
 }
